40ad34565d013ef0184d1271598aa1097ce1f0b6
Alexander-Acu <alexanderacuna1984@gmail.com>
2025-05-29 11:12:37 -0600

    Agregando proyecto de pruebas unitarias

---
+++ ApiLafise.Tests/ClienteTests.cs
@@ -0,0 +1,27 @@
+﻿using ApiLafise.Models;
+using ApiLafise.Repositories;
+using Xunit;
+
+namespace ApiLafise.Tests
+{
+
+    //Prueba unitaria para el segmento de Cliente
+    public class ClienteTests
+    {
+        [Fact]
+        public void InsertarCliente_DeberiaRetornarTrue()
+        {
+            var clienteRepo = new ClienteRepository();
+            var cliente = new Cliente
+            {
+                Nombre = "Prueba_" + Guid.NewGuid().ToString("N").Substring(0, 8),
+                FechaNacimiento = new DateTime(1990, 1, 1),
+                Sexo = "M",
+                Ingresos = 3000
+            };
+        
+            var resultado = clienteRepo.InsertarCliente(cliente);
+            Assert.True(resultado);
+        }
+    }
+}
---
+++ ApiLafise.Tests/CuentaTests.cs
@@ -0,0 +1,31 @@
+﻿using ApiLafise.Models;
+using ApiLafise.Repositories;
+using Xunit;
+
+namespace ApiLafise.Tests
+{
+    //Prueba unitaria para la creacion de cuentas bancarias
+    public class CuentaTests
+    {
+        [Fact]
+        public void InsertarCuenta_DeberiaRetornarTrue()
+        {
+            string numeroCuentaUnico = "TEST-" + Guid.NewGuid().ToString("N").Substring(0, 8);
+            var cuentaRepo = new CuentaRepository();
+            cuentaRepo.InsertarCuenta(new CuentaBancaria
+            {
+                NumeroCuenta = numeroCuentaUnico,
+                ClienteId = 1,
+                Saldo = 10
+            });
+
+            var transaccion = new Transaccion
+            {
+                NumeroCuenta = numeroCuentaUnico, // Usar el mismo valor generado
+                Tipo = "Retiro",
+                Monto = 999999
+            };
+
+        }
+    }
+}
